e3fc8d34dcf17c52a7b14068058bd4c216301d5d
Anton Tymchuk <antontymchuk@MacBook-Air-Anton.local>
2015-05-21 23:56:59 +0300

    Menu from 990px

---
+++ app/scripts/main.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    'use strict';
+
+    $('.header--fixed').find('.header--hidden').css({ display: 'none' });
+    // $('.header--fixed').find('.header--hidden').addClass('hidden');
+});
