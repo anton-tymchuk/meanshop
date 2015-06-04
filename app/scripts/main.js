070b44c8f511f69a6dc0d21178a4d78c40b605f1
Anton Tymchuk <antontymchuk@MacBook-Air-Anton.local>
2015-06-04 11:05:54 +0300

    Home page is ready

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,5 +2,4 @@ $(document).ready(function () {
     'use strict';
 
     $('.header--fixed').find('.header--hidden').css({ display: 'none' });
-    // $('.header--fixed').find('.header--hidden').addClass('hidden');
 });
