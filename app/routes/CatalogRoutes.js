dbd034fdd5f95b51c3c8dd8ff0c1d50190d2a549
Anton Tymchuk <antontymchuk@MacBook-Air-Anton.local>
2015-06-10 11:28:26 +0300

    Fetch category listing Api

--- app/routes/CatalogRoutes.js
+++ app/routes/CatalogRoutes.js
@@ -4,7 +4,22 @@ var routes = function (Product) {
     var catalogRouter = express.Router();
 
     //TODO: Get all categories list
+    catalogRouter.route('/')
+        .get(function (req, res) {
+            Product.find({}, function (err, cats) {
+                var catArray = [];
 
+                for(var i = 0, j = cats.length; i < j; i++){
+                    var current = cats[i].type
+
+                    if(catArray.indexOf(current) < 0){
+                        catArray.push(current);
+                    }
+                }
+
+                res.json(catArray);
+            });
+        });
 
     // Fetch products from a category
     catalogRouter.use('/:catName', function (req, res, next) {
